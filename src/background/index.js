8b9ce1a7485a5db68ebb786b54bfc0c4941d6a35
tangyu <yu.tang02@hand-china.com>
2020-12-22 11:14:41 +0800

    fix: 一些修改

--- src/background/index.js
+++ src/background/index.js
@@ -1,13 +1,37 @@
-/* globals chrome */
-import { toggle } from "@/common";
+/* globals chrome browser */
 
-chrome.commands.onCommand.addListener((command) => {
-  if (command === "toggle-xpath") {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
-      toggle(tab[0]);
-    });
+const state = {
+  loaded: {},
+  injected: {},
+};
+
+var platform = typeof browser === "undefined" ? chrome : browser;
+
+const toggleIn = ({ id: tab_id }) => {
+  // toggle out: it's currently loaded and injected
+  if (state.loaded[tab_id] && state.injected[tab_id]) {
+    platform.tabs.executeScript(tab_id, { file: "content/eject.js" });
+    state.injected[tab_id] = false;
   }
-});
 
-// chrome.tabs.onUpdated.addListener(getActiveTab);
-// chrome.browserAction.onClicked.addListener(toggle);
+  // toggle in: it's loaded and needs injected
+  else if (state.loaded[tab_id] && !state.injected[tab_id]) {
+    platform.tabs.executeScript(tab_id, { file: "content/restore.js" });
+    state.injected[tab_id] = true;
+    // getColorMode();
+  }
+
+  // fresh start in tab
+  else {
+    // platform.tabs.insertCSS(tab_id, { file: "toolbar/bundle.css" });
+    platform.tabs.executeScript(tab_id, { file: "content/inject.js" });
+
+    state.loaded[tab_id] = true;
+    state.injected[tab_id] = true;
+    // getColorMode();
+  }
+
+  platform.tabs.onUpdated.addListener(function (tabId) {
+    if (tabId === tab_id) state.loaded[tabId] = false;
+  });
+};
